47005986ced7dc8ae178913b56c610c4e9e0e350
austenjs <e0407786@u.nus.edu>
2022-02-28 03:37:42 +0800

    remove texts

--- HW2/testQueryEvaluator.py
+++
@@ -1,9 +0,0 @@
-from queryParser import QueryParser
-from queryEvaluator import QueryEvaluator
-
-parser = QueryParser()
-evaluator = QueryEvaluator()
-
-queries = parser.parse_queries('validQueries.txt')
-for query in queries:
-    print(evaluator.evaluate(query))
--- HW2/testQueryParser.py
+++
@@ -1,9 +0,0 @@
-from queryParser import QueryParser
-
-parser = QueryParser()
-
-print('Invalid Queries')
-parser.parse_queries('invalidQueries.txt')
-
-print('Valid Queries')
-print(parser.parse_queries('validQueries.txt'))
